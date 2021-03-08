cf8a020f025d6ade172ebf6f0403c68b3f82eeca
DX1005 <73885254+ZFeng3242@users.noreply.github.com>
2021-03-08 18:57:54 +0800

    Update jd_global.js

--- jd_global.js
+++ jd_global.js
@@ -311,7 +311,7 @@ function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
     $.get({
-      url: `http://jd.turinglabs.net/api/v2/jd/jdglobal/read/${randomCount}/`,
+      url: `https://github.com/ZFeng3242/RandomShareCode/raw/main/JD_Global.json`,
       'timeout': 10000
     }, (err, resp, data) => {
       try {
@@ -320,7 +320,7 @@ function readShareCode() {
           console.log(`${$.name} API请求失败，请检查网路重试`)
         } else {
           if (data) {
-            console.log(`随机取${randomCount}个码放到您固定的互助码后面(不影响已有固定互助)`)
+            console.log(`随机取固定助力码放到您的互助码后面(不影响已有固定互助)`)
             data = JSON.parse(data);
           }
         }
