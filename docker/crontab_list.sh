8abebd2df23f102c6c6d5ee6f232b8e5d937d95f
DX1005 <73885254+ZFeng3242@users.noreply.github.com>
2021-03-17 08:35:03 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -19,6 +19,8 @@
 35 6,22 * * * node /scripts/jd_global.js >> /scripts/logs/jd_global.log 2>&1
 #京东众筹许愿池
 10 10,15 13-20 3 * node /scripts/jd_wish.js >> /scripts/logs/jd_wish.log 2>&1
+#手机尚学季
+0 0,14 * * * node /scripts/jd_xtg.js >> /scripts/logs/jd_xtg.log 2>&1
 
 ##############长期活动##############
 # 签到
