beace8fc361f307c05e9ccf2e93bcba4e0a0eb98
ZFeng3242 <771849136@qq.com>
2021-05-10 14:12:00 +0800

    修复美丽研究院以及取消关注

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -23,6 +23,8 @@
 30 0,1-23/1 * * * node /scripts/jd_half_redrain.js >> /scripts/logs/jd_half_redrain.log 2>&1
 #jd_daily_lottery
 13 1,22,23 * * * node /scripts/jd_daily_lottery.js >> /scripts/logs/jd_daily_lottery.log 2>&1
+#jd_beauty
+1 7,12,19 * * * node /scripts/jd_beauty.js >> /scripts/logs/jd_beauty.log 2>&1
 
 ##############长期活动##############
 # 签到
