ba6551164258dc707787fa85a4da38640be9c1af
DX1005 <73885254+ZFeng3242@users.noreply.github.com>
2021-05-28 02:56:30 +0800

    Update jd_zoo.js

--- jd_zoo.js
+++ jd_zoo.js
@@ -38,7 +38,7 @@ $.innerPkInviteList = [
   'sSKNX-MpqKOJsNu_m5iLABwjHESM4ggMoWnE4jQolb-gghsoSHxSyMzO2Sgw8ew',
   'sSKNX-MpqKOJsNvA49iYfF0vk-VP4OATmHFwc7stga4jeQDzXm3VORjKXNvu',
   'sSKNX-MpqKOJsNu8nc6NA88mD03SR4re2f2oEOwXhwKldIQk7jmmFODK2ROPUVI',
-  'sSKNX-MpqKMHbAlvNg4AtbPk2pygMqWs0IwxO5kKoWMYVodHVTh2'
+  'sSKNX-MpqKMHbAlvNg4AtbPk2pygMqWs0IwxO5kKoWMYVodHVTh5'
 ];
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
