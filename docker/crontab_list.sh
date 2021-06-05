dc50be3f8335566f02af8d43e5c47295f27bfa86
ZFeng3242 <771849136@qq.com>
2021-06-05 13:52:04 +0800

    fix
    删除zoo无用接口，星店长增加，删除京喜签到

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -126,8 +126,6 @@
 0 9,12,18 * * * node /scripts/jd_jxnc.js >> /scripts/logs/jd_jxnc.log 2>&1
 # 签到领现金
 27 7,15 * * * node /scripts/jd_cash.js >> /scripts/logs/jd_cash.log 2>&1
-# 京喜app签到
-39 7 * * * node /scripts/jx_sign.js >> /scripts/logs/jx_sign.log 2>&1
 # 闪购盲盒
 27 8 * * * node /scripts/jd_sgmh.js >> /scripts/logs/jd_sgmh.log 2>&1
 # 京东秒秒币
